4d7652a9329441a0b1fc04bdd08039fa80ffaf1f
Nikhil Srivastava <nikhilsrivastava41@gmail.com>
2020-06-21 14:05:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./components/TodoList";
+import TodoInput from "./components/TodoInput";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>Todo app</h1>
+        <TodoInput />
+        <TodoList />
+      </div>
+    );
+  }
 }
-
-export default App;
